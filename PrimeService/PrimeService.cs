6110d5209eb59a98fe2b5fb42e5186f7d539c22d
Евгений Власов <zhenya.vlasov2002@mail.ru>
2023-07-03 16:51:47 +0300

    Добавьте файлы проекта.

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,82 @@
+﻿namespace PrimeService;
+
+public class PrimeService
+{
+    public bool isPrime(int candidate)
+    {
+        if (candidate < 2)
+            return false;
+        throw new NotImplementedException("Please create a test first.");
+    }
+
+    public int MajorityElement(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        int maxValue = int.MinValue;
+        int maxKey = int.MinValue;
+        bool flag = true;
+        foreach (var i in nums)
+        {
+            if (dict.ContainsKey(i))
+            {
+                dict[i]++;
+                if (maxValue < dict[i])
+                {
+                    maxValue = dict[i];
+                    maxKey = i;
+                }
+            }
+            else
+            {
+                dict[i] = 1;
+                if (flag)
+                {
+                    maxValue = dict[i];
+                    maxKey = i;
+                    flag = false;
+                }
+            }
+        }
+
+        return maxKey;
+    }
+
+    public void Rotate(int[] nums, int k)
+    {
+        k = nums.Length % k;
+        int counter = 0;
+        while (counter != k)
+        {
+            int lastElemt = nums.Last();
+            int size = nums.Length - 1;
+            for (int i = size; i > 0; i--)
+            {
+                nums[i] = nums[i - 1];
+            }
+            counter++;
+            nums[0] = lastElemt;
+        }
+    }
+
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+        int minValue = int.MaxValue;
+
+        foreach(int i in prices)
+        {
+            if (i < minValue) minValue = i;
+            else if (i - minValue > maxProfit) maxProfit = i - minValue;
+        }
+
+        return maxProfit;
+        
+    }
+
+    public int LengthOfLastWorld(string s)
+    {
+        string[] words = s.Split(' ' , StringSplitOptions.RemoveEmptyEntries); ;
+
+        return words[words.Length-1].Length;
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,57 @@
+namespace Tests;
+
+
+[TestFixture]
+public class Tests
+{
+    private PrimeService.PrimeService _primeService;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _primeService = new PrimeService.PrimeService();
+    }
+
+    [TestCase(-1)]
+    [TestCase(0)]
+    [TestCase(1)]
+    public void Test1(int value)
+    {
+        var result = _primeService.isPrime(value);
+
+        Assert.IsFalse(result, "1 should not be prime");
+    }
+
+    [TestCase(new int[] { 3, 2, 3},3)]
+    [TestCase(new int[] { 2, 2, 1, 1, 1, 2, 2 },2)]
+    public void TestMajority(int[] array, int resultTrue)
+    {
+        var result = _primeService.MajorityElement(array);
+        Assert.AreEqual(result, resultTrue);
+    }
+
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3, new int[] { 5, 6, 7, 1, 2, 3, 4 })]
+    [TestCase(new int[] { -1, -100, 3, 99 }, 2, new int[] { 3, 99, -1, -100 })]
+    public void TestRotate(int[] array, int k, int[] result)
+    {
+        _primeService.Rotate(array,k);
+        Assert.AreEqual(array, result);
+    }
+
+    [TestCase(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+    [TestCase(new int[] { 7, 6, 4, 3, 1 }, 0)]
+    public void TestMaxProfit(int[] prices, int result)
+    {
+        var resultMethod = _primeService.MaxProfit(prices);
+        Assert.AreEqual(resultMethod, result);
+    }
+
+    [TestCase("Hello World", 5)]
+    [TestCase("   fly me   to   the moon  ", 4)]
+    [TestCase("luffy is still joyboy", 6)]
+    public void TestLengthOfLastWord(string s, int result)
+    {
+        var resultFund = _primeService.LengthOfLastWorld(s);
+        Assert.AreEqual(resultFund, result);
+    }
+}
